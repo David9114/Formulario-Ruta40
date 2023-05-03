8d5936c94c0344954bcea05d9858e50332a9819f
Kroonozz <David.s.1491@gmail.com>
2023-05-03 09:55:29 -0500

    Cambios ID

--- Main.js
+++ Main.js
@@ -1,15 +1,24 @@
 next = document.querySelector("#next"),
-back = document.querySelector("#back"),
-back1 = document.querySelector("#back1"),
-condiciones = document.querySelector("#condiciones"),
-general = document.querySelector("#general"),
-next1 = document.querySelector("#next1"),
-adjuntos = document.querySelector("#adjuntos");
+  back = document.querySelector("#back"),
+  back1 = document.querySelector("#back1"),
+  condiciones = document.querySelector("#condiciones"),
+  general = document.querySelector("#general"),
+  next1 = document.querySelector("#next1"),
+  adjuntos = document.querySelector("#adjuntos");
 
 function mostrar() {
   console.log(document.getElementById("seleccionar").value);
-  console.log(document.getElementById("seleccionar").value);
-  
+  console.log(document.getElementById("nitEmpresa").value);
+  console.log(document.getElementById("nombreContratista").value);
+  console.log(document.getElementById("representanteLegal").value);
+  console.log(document.getElementById("direccion").value);
+  console.log(document.getElementById("cuidad").value);
+  console.log(document.getElementById("departamento").value);
+  console.log(document.getElementById("telefonoFijo").value);
+  console.log(document.getElementById("telefonoMovil").value);
+  console.log(document.getElementById("fax").value);
+  console.log(document.getElementById("email").value);
+  console.log(document.getElementById("sectorEconomico").value);
 
   if (next.value == 1) {
     condiciones.classList.remove("d-none");
