19a649c3379e42858f45c6f1a4da596797698f1b
Kroonozz <David.s.1491@gmail.com>
2023-05-05 10:35:14 -0500

    cambios funcionalidad validacionn de campos nulos

--- Main.js
+++ Main.js
@@ -8,40 +8,65 @@ next = document.querySelector("#next"),
 nitEmpresa = document.querySelector("#nitEmpresa");
 
 function mostrar() {
-  console.log(document.getElementById("seleccionar").value);
-  console.log(document.getElementById("nitEmpresa").value);
-  console.log(document.getElementById("nombreContratista").value);
-  console.log(document.getElementById("representanteLegal").value);
-  console.log(document.getElementById("direccion").value);
-  console.log(document.getElementById("cuidad").value);
-  console.log(document.getElementById("departamento").value);
-  console.log(document.getElementById("telefonoFijo").value);
-  console.log(document.getElementById("telefonoMovil").value);
-  console.log(document.getElementById("fax").value);
-  console.log(document.getElementById("email").value);
-  console.log(document.getElementById("sectorEconomico").value);
+  // console.log(document.getElementById("seleccionar").value);
+  // console.log(document.getElementById("nitEmpresa").value);
+  // console.log(document.getElementById("nombreContratista").value);
+  // console.log(document.getElementById("representanteLegal").value);
+  // console.log(document.getElementById("direccion").value);
+  // console.log(document.getElementById("cuidad").value);
+  // console.log(document.getElementById("departamento").value);
+  // console.log(document.getElementById("telefonoFijo").value);
+  // console.log(document.getElementById("telefonoMovil").value);
+  // console.log(document.getElementById("fax").value);
+  // console.log(document.getElementById("email").value);
+  // console.log(document.getElementById("sectorEconomico").value);
 
+
+}
+
+const forms = document.querySelectorAll('.needs-validation')
+
+
+Array.from(forms).forEach(form => {
+  next.addEventListener('click', event => {
+    if (!form.checkValidity()) {
+      event.preventDefault()
+      event.stopPropagation()
+
+    }
+
+    form.classList.add('was-validated')
+  }, false)
   if (next.value == 1) {
     condiciones.classList.remove("d-none");
     general.classList.add("d-none");
   }
-}
-
-nitEmpresa.addEventListener('blur', (eNit) => {
-  console.log("Evento blur");
-  const nit = eNit.target,
-    nitValue = eNit.target.value;
-  if (nitValue.length === 0) {
-    nit.classList.add("invalid");
-    nit.nextElementSibling.classList.add("error");
-    nit.nextElementSibling.innerText = "Por favor ingrese el campo Nit Empresa"
-    console.log("prueba");
-
-  }
-
-  console.log(nit);
-
 })
+
+
+
+
+
+
+
+
+
+
+// nitEmpresa.addEventListener('blur', (eNit) => {
+//   console.log("Evento blur");
+//   const nit = eNit.target,
+//     nitValue = eNit.target.value;
+//   if (nitValue.length === 0) {
+//     nit.classList.add("invalid");
+//     nit.nextElementSibling.classList.add("error");
+//     nit.nextElementSibling.innerText = "Por favor ingrese el campo Nit Empresa"
+//     console.log("prueba");
+
+//   }
+
+//   console.log(nit);
+
+// })
 
 
 
