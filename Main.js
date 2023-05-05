3849a315b9f7ec5e199980919daeaf0343893e39
Kroonozz <David.s.1491@gmail.com>
2023-05-05 12:53:35 -0500

    Cambios Js

--- Main.js
+++ Main.js
@@ -6,26 +6,10 @@ next = document.querySelector("#next"),
   next1 = document.querySelector("#next1"),
   adjuntos = document.querySelector("#adjuntos");
 nitEmpresa = document.querySelector("#nitEmpresa");
-
-function mostrar() {
-  // console.log(document.getElementById("seleccionar").value);
-  // console.log(document.getElementById("nitEmpresa").value);
-  // console.log(document.getElementById("nombreContratista").value);
-  // console.log(document.getElementById("representanteLegal").value);
-  // console.log(document.getElementById("direccion").value);
-  // console.log(document.getElementById("cuidad").value);
-  // console.log(document.getElementById("departamento").value);
-  // console.log(document.getElementById("telefonoFijo").value);
-  // console.log(document.getElementById("telefonoMovil").value);
-  // console.log(document.getElementById("fax").value);
-  // console.log(document.getElementById("email").value);
-  // console.log(document.getElementById("sectorEconomico").value);
-
-
-}
-
 const forms = document.querySelectorAll('.needs-validation')
 
+function mostrar() {
+}
 
 Array.from(forms).forEach(form => {
   next.addEventListener('click', event => {
@@ -37,39 +21,13 @@ Array.from(forms).forEach(form => {
 
     form.classList.add('was-validated')
   }, false)
+
+  
   if (next.value == 1) {
     condiciones.classList.remove("d-none");
     general.classList.add("d-none");
   }
 })
-
-
-
-
-
-
-
-
-
-
-// nitEmpresa.addEventListener('blur', (eNit) => {
-//   console.log("Evento blur");
-//   const nit = eNit.target,
-//     nitValue = eNit.target.value;
-//   if (nitValue.length === 0) {
-//     nit.classList.add("invalid");
-//     nit.nextElementSibling.classList.add("error");
-//     nit.nextElementSibling.innerText = "Por favor ingrese el campo Nit Empresa"
-//     console.log("prueba");
-
-//   }
-
-//   console.log(nit);
-
-// })
-
-
-
 
 function mostrar2() {
   if (back.value == 0) {
@@ -94,6 +52,5 @@ function mostrar4() {
   }
 }
 
-function validarFormulario() {
-  alert("Todo en orden");
-}
+
+
