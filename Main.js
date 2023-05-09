ac49cfafbc6106776b9552cec09962d7c5681be5
David Escudero Cortés <David.s.1491@gmail.com>
2023-05-08 22:21:44 -0500

    Cambios funcionalidad

--- Main.js
+++ Main.js
@@ -1,29 +1,29 @@
-next = document.querySelector("#next"),
-  back = document.querySelector("#back"),
-  back1 = document.querySelector("#back1"),
-  condiciones = document.querySelector("#condiciones"),
-  general = document.querySelector("#general"),
-  next1 = document.querySelector("#next1"),
-  adjuntos = document.querySelector("#adjuntos");
+next = document.querySelector("#next");
+back = document.querySelector("#back");
+back1 = document.querySelector("#back1");
+condiciones = document.querySelector("#condiciones");
+general = document.querySelector("#general");
+next1 = document.querySelector("#next1");
+adjuntos = document.querySelector("#adjuntos");
 nitEmpresa = document.querySelector("#nitEmpresa");
+forms = document.querySelectorAll(".needs-validation");
+formularioUnoValido = false;
 
-
-const forms = document.querySelectorAll('.needs-validation')
-
-Array.from(forms).forEach(form => {
-  next.addEventListener('click', event => {
+Array.from(forms).forEach((form) => {
+  next.addEventListener("click", (event) => {
     if (!form.checkValidity()) {
-      event.preventDefault()
-      event.stopPropagation()
-      console.log("Prueba");
+      event.preventDefault();
+      event.stopPropagation();
+      formularioUnoValido = false;
     }
+    form.classList.add("was-validated");
+    formularioUnoValido = true;
+  });
+});
 
-    form.classList.add('was-validated')
-  }, false)
-})
-
-function siguiente() {
-  if (next.value == 1) {
+function mostrar() {
+  console.log("Prueba", forms);
+  if (next.value == 1 && formularioUnoValido) {
     condiciones.classList.remove("d-none");
     general.classList.add("d-none");
   }
@@ -51,6 +51,3 @@ function mostrar4() {
     adjuntos.classList.add("d-none");
   }
 }
-
-
-
